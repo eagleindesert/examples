2bf999c6165e25a10b82edb69cd22ff484871948
eagleIndesert <121093691+eagleIndesert@users.noreply.github.com>
2025-08-29 21:55:47 +0900

    1일1

--- backjoon/1436/test.py
+++ backjoon/1436/test.py
@@ -1,30 +1,11 @@
-# 리스트 입력 후 리스트 출력
-def add_1(lst: list):
+nums = [] 
 
-    # 문자열 뒤집어서 생각
-    lst.reverse()
+n = int(input())
 
-    # 문자열 덧셈 개시
-    i = 0
-    carry = True
-    while i < len(lst) and carry == True:
-        if lst[i] != "9":
-            lst[i] = chr(ord(lst[i]) + 1)
-            carry = False
-        else:
-            lst[i] = "0"
-            carry = True
-        i += 1
+for i in range (1, n+1):
+    nums.append(i)
 
-    # 모든 자리가 9999.. 일때 마지막 1
-    if carry == True:
-        lst.append("1")
+example = {}
+example[1]=33
 
-    # 원상 복구
-    lst.reverse()
-
-    # 리스트 리턴
-    return lst
-
-
-print(add_1(['9']))
+print(example)
