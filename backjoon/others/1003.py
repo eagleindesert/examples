dc114aeef2f2ca5fadaa6b209a8d0391f6892f61
eagleIndesert <toridoremi@naver.com>
2025-09-28 19:42:02 +0900

    1003, DP, 피보나치

--- backjoon/others/1003.py
+++ backjoon/others/1003.py
@@ -1,7 +1,39 @@
 import sys
+from collections import defaultdict
+
+
+def fibo(n, cnt, dpList):
+    if n in dpList:
+        return dpList[n]
+
+    if n == 0:
+        cnt[0] = {0: 1, 1: 0}
+        return 0
+    elif n == 1:
+        cnt[1] = {0: 0, 1: 1}
+        return 1
+    else:
+        branchSum = fibo(n - 1, cnt, dpList) + fibo(n - 2, cnt, dpList)
+        dpList[n] = branchSum
+
+        cnt[n] = {0: 0, 1: 0}
+        cnt[n][0] = cnt[n - 1][0] + cnt[n - 2][0]
+        cnt[n][1] = cnt[n - 1][1] + cnt[n - 2][1]
+
+        return branchSum
+
 
 def main():
-    
+    dpList = defaultdict()
+
+    cnt = defaultdict()
+
+    T = int(sys.stdin.readline())
+
+    for _ in range(T):
+        N = int(sys.stdin.readline())
+        fibo(N, cnt, dpList)
+        print(cnt[N][0], cnt[N][1])
 
 
 main()
