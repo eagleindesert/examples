e050616b798b75e03ab0f69efb59188d4b6dc1fc
eagleIndesert <toridoremi@naver.com>
2025-10-31 11:28:48 +0900

    11726, DP

--- backjoon/others/11726.py
+++ backjoon/others/11726.py
@@ -1,7 +1,19 @@
 import sys
+from collections import defaultdict
+
 
 def main():
-    
+    valueMem = defaultdict(int)
+
+    n = int(sys.stdin.readline())
+
+    valueMem[1] = 1
+    valueMem[2] = 2
+
+    for i in range(3, n + 1):
+        valueMem[i] = valueMem[i - 1] + valueMem[i - 2]
+
+    print(valueMem[n] % 10007)
 
 
 main()
