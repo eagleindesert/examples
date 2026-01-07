abd50c84742bc0ea36defa619970b861463463cb
eagleIndesert <toridoremi@naver.com>
2026-01-07 20:51:18 +0900

    1074, 이진탐색 응용, 변수 오타 주의!

--- backjoon/1074/1074-2.py
+++ backjoon/1074/1074-2.py
@@ -1,8 +1,76 @@
 import sys
+from collections import defaultdict
+
 
 def main():
     N, r, c = map(int, input().split())
 
-    
+    multi_num = defaultdict(int)
+
+    length = 2**N
+
+    low = 0
+    high = length - 1
+    mid = (low + high) // 2
+
+    low_r = low
+    high_r = high
+    mid_r = mid
+
+    low_c = low
+    high_c = high
+    mid_c = mid
+
+    cur = N
+
+    while low_r < high_r and low_c < high_c:
+        if low_r <= r <= mid_r and low_c <= c <= mid_c:
+            multi_num[cur] = 0
+
+            high_r = mid_r
+            high_c = mid_c
+
+            # print("0곱")
+
+        elif low_r <= r <= mid_r and mid_c < c <= high_c:
+            multi_num[cur] = 1
+
+            high_r = mid_r
+            low_c = mid_c + 1
+
+            # print("1곱")
+
+        elif mid_r < r <= high_r and mid_c < c <= high_c:
+            multi_num[cur] = 3
+
+            low_r = mid_r + 1
+            low_c = mid_c + 1
+
+            # print("3곱")
+
+        elif mid_r < r <= high_r and low_c <= c <= mid_c:
+            multi_num[cur] = 2
+
+            low_r = mid_r + 1
+            high_c = mid_c
+
+            # print("2곱")
+
+        else:
+            print("이상함")
+
+        mid_r = (low_r + high_r) // 2
+        mid_c = (low_c + high_c) // 2
+
+        cur -= 1
+
+    result_sum = multi_num[1]
+    for i in range(2, N + 1):
+        result_sum += (4 ** (i - 1)) * multi_num[i]
+        # print(result_sum)
+
+    print(result_sum)
+    # print(multi_num)
+
 
 main()
