786da6506f9539032569101eff96cdb6bffe71c9
eagleIndesert <toridoremi@naver.com>
2026-01-15 18:16:48 +0900

    11725S. DFS를 데크로 구현. 코드의 순서 및 분기를 적절히 나누는 것을 중점으로 하여 작성.

--- backjoon/11725/11725-2.py
+++ backjoon/11725/11725-2.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 def main():
     N = int(sys.stdin.readline())
     graph = [set() for _ in range(N + 1)]
+    age = [0] * (N + 1)
 
     for i in range(N - 1):
         v1, v2 = map(int, sys.stdin.readline().split())
@@ -15,23 +16,33 @@ def main():
         graph[v1].add(v2)
         graph[v2].add(v1)
 
-    print(graph)
+    # print(graph)
 
-    dfs(1, graph)
+    dfs(1, graph, age)
+
+    #print(f"각 노드의 깊이 = {age}")
+
+    for node in range(2, N + 1):
+        for target_node in list(graph[node]):
+            if age[node] > age[target_node]:
+                print(target_node)
+                break
 
 
-def dfs(start, graph):
-    stack = [start]
+def dfs(start, graph, age):
+    stack = deque([start])
     visited = set()
 
     while stack:
         node = stack.pop()
+
         if node not in visited:
-            print(f"방문 {node}")
+            # print(f"노드 방문 {node}")
             visited.add(node)
 
             for neighbor in reversed(list(graph[node])):
                 if neighbor not in visited:
+                    age[neighbor] = age[node] + 1
                     stack.append(neighbor)
 
 
