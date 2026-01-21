7849a1445e634795e24e683901a8092daf0ebf66
eagleIndesert <toridoremi@naver.com>
2026-01-21 18:11:28 +0900

    9465S. 런타임 오류 수정.

--- backjoon/others/9465.py
+++ backjoon/others/9465.py
@@ -15,15 +15,18 @@ def main():
         dp = [[-1] * n, [-1] * n]
 
         dp[0][0] = input_list[0][0]
-        dp[0][1] = input_list[0][1] + input_list[1][0]
         dp[1][0] = input_list[1][0]
-        dp[1][1] = input_list[1][1] + input_list[0][0]
+
+        # 입력 케이스 N = 1인 경우를 고려하여
+        if n > 1:
+            dp[0][1] = input_list[0][1] + input_list[1][0]
+            dp[1][1] = input_list[1][1] + input_list[0][0]
 
         for j in range(2, n):
             dp[0][j] = input_list[0][j] + max(dp[1][j - 1], dp[1][j - 2])
             dp[1][j] = input_list[1][j] + max(dp[0][j - 1], dp[0][j - 2])
 
-        print(max(dp[0][n-1], dp[1][n-1]))
+        print(max(dp[0][n - 1], dp[1][n - 1]))
 
 
 main()
