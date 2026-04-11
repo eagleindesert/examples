45f7bc144e05a44b52ca1b35768cdcc205a7f2a5
eagleIndesert <toridoremi@naver.com>
2026-04-12 01:35:34 +0900

    1865S, 벨만포드 알고리즘 기초

--- backjoon/others/1865.py
+++ backjoon/others/1865.py
@@ -1,25 +1,54 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 
-def BFS(graph):
-    print("abc")
+def bellman_ford(N, edges):
+    dist = [0] * (N + 1)
 
+    for i in range(N):
+        for v1, v2, w in edges:
+            if dist[v1] + w < dist[v2]:
+                dist[v2] = dist[v1] + w
+
+                if i == N - 1:
+                    return True
+
+    return False
+
+
+"""
+테스트 케이스 우선 1개로 고정
+
+1
+3 3 1
+1 2 2
+1 3 4
+2 3 1
+3 1 4
+
+"""
 
 if __name__ == "__main__":
     TC = int(input())
-    N, M, W = map(int, input().split())
 
-    # [[empty], [(node1, w1), ..], ..]
-    graph = [[] for i in range(N + 1)]
+    for _ in range(TC):
+        # M은 edges, W는 웜홀
+        N, M, W = map(int, input().split())
 
-    for i in range(TC):
-        for j in range(M):
-            a, b, c = map(int, input().split())
-            graph[a].append((b, c))
-        for j in range(W):
-            a, b, c = map(int, input().split())
-            graph[a].append((b, c))
+        edges = []
 
-    print(graph)
+        # 도로 추가
+        for _ in range(M):
+            v1, v2, w = map(int, input().split())
+            edges.append((v1, v2, w))
+            edges.append((v2, v1, w))
+
+        # 웜홀 추가
+        for _ in range(W):
+            v1, v2, w = map(int, input().split())
+            edges.append((v1, v2, -w))
+
+        # print(edges)
+        print("YES") if bellman_ford(N, edges) else print("NO")
